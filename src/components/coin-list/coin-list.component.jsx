6da6a42c3b9711b0c73d6003ec1648e511f41435
Allan Gabriel <allan.gabriel@protonmail.com>
2020-09-25 18:33:32 -0300

    Add nothing found message

--- src/components/coin-list/coin-list.component.jsx
+++ src/components/coin-list/coin-list.component.jsx
@@ -5,12 +5,15 @@ import "./coin-list.styles.css";
 export const CoinList = ({ coins }) => {
     return (
         <div className="coin-list">
-            {coins.map(coin => 
+            {coins.length > 0 ? coins.map(coin => 
                 <CoinCard
                     key={ coin.uuid } 
                     coin={ coin }
                 />
-            )}
+            ) : 
+            <div className="not-found">
+                <h1>Nothing Found :/</h1>
+            </div>}
         </div>
     );
 }
